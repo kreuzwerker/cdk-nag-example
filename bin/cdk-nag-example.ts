a81c54487a2ff8bb8d86bab8c433dc9804d8dd84
frank.wesemann <frank.wesemann@kreuzwerker.de>
2022-07-11 18:29:04 +0200

    introduce the checks

--- bin/cdk-nag-example.ts
+++ bin/cdk-nag-example.ts
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {CdkNagExampleStack} from '../lib/cdk-nag-example-stack';
+import { CdkNagExampleStack } from '../lib/cdk-nag-example-stack';
+import {Aspects} from "aws-cdk-lib";
+import {AwsSolutionsChecks, HIPAASecurityChecks} from "cdk-nag";
+import {NIST80053R4Checks} from "cdk-nag/lib/packs/nist-800-53-r4";
+import {NIST80053R5Checks} from "cdk-nag/lib/packs/nist-800-53-r5";
+import {PCIDSS321Checks} from "cdk-nag/lib/packs/pci-dss-321";
 
 const app = new cdk.App();
 new CdkNagExampleStack(app, 'CdkNagExampleStack', {});
+
+Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}));
+//Aspects.of(app).add(new HIPAASecurityChecks({verbose:true}));
+//Aspects.of(app).add(new NIST80053R4Checks({verbose:true}));
+//Aspects.of(app).add(new NIST80053R5Checks({verbose:true}));
+//Aspects.of(app).add(new PCIDSS321Checks({verbose:true}));
