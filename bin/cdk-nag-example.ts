fd5cbae7e31843dd68b8116e3419f64e03ef5bdc
frank.wesemann <frank.wesemann@kreuzwerker.de>
2022-07-12 19:59:46 +0200

    add suppressions and log them

--- bin/cdk-nag-example.ts
+++ bin/cdk-nag-example.ts
@@ -9,10 +9,14 @@ import {NIST80053R5Checks} from 'cdk-nag/lib/packs/nist-800-53-r5';
 import {PCIDSS321Checks} from 'cdk-nag/lib/packs/pci-dss-321';
 
 const app = new cdk.App();
-new CdkNagExampleStack(app, 'CdkNagExampleStack', {});
+const stack = new CdkNagExampleStack(app, 'CdkNagExampleStack', {});
 
-Aspects.of(app).add(new AwsSolutionsChecks({verbose: true}));
-//Aspects.of(app).add(new HIPAASecurityChecks({verbose:true}));
-//Aspects.of(app).add(new NIST80053R4Checks({verbose:true}));
-//Aspects.of(app).add(new NIST80053R5Checks({verbose:true}));
-//Aspects.of(app).add(new PCIDSS321Checks({verbose:true}));
+Aspects.of(app).add(new AwsSolutionsChecks({verbose: true, logIgnores: true}));
+//Aspects.of(app).add(new HIPAASecurityChecks({verbose:true, logIgnores: true}));
+//Aspects.of(app).add(new NIST80053R4Checks({verbose:true, logIgnores: true}));
+//Aspects.of(app).add(new NIST80053R5Checks({verbose:true, logIgnores: true}));
+//Aspects.of(app).add(new PCIDSS321Checks({verbose:true, logIgnores: true}));
+NagSuppressions.addStackSuppressions(stack, [
+  { id: 'AwsSolutions-IAM4', reason: 'We prefer the AWS managed policies' },
+  { id: 'AwsSolutions-IAM5', reason: 'We prefer the AWS managed roles' },
+]);
